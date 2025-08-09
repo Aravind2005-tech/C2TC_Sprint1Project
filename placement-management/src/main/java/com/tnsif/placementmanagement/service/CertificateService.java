75a0ae9ad84aed936861354cd60d733f2ec27bef
Aravind.V <191790113+Aravind2005-tech@users.noreply.github.com>
2025-08-09 22:12:45 +0530

    This file contain sprint 1 project.

--- placement-management/src/main/java/com/tnsif/placementmanagement/service/CertificateService.java
+++ placement-management/src/main/java/com/tnsif/placementmanagement/service/CertificateService.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public class CertificateService {
     private final CertificateRepository certificateRepository;
 
+    
     public CertificateService(CertificateRepository certificateRepository) {
         this.certificateRepository = certificateRepository;
     }
